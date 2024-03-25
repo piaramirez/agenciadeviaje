38850e949d62a7c87ec8349bdfc5fe3b3688169b
Pía Ramírez <the.captain312@gmail.com>
2024-03-25 13:24:28 -0600

    Solucionando problemas

--- agenciadeviaje/resources/views/admin/body/footer.blade.php
+++ agenciadeviaje/resources/views/admin/body/footer.blade.php
@@ -2,12 +2,11 @@
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col-sm-6">
-                                <script>document.write(new Date().getFullYear())</script> © Upcube.
+                               Dr Ñ © Upcube.
                             </div>
                             <div class="col-sm-6">
                                 <div class="text-sm-end d-none d-sm-block">
-                                    Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesdesign
-                                </div>
+                                   Por doctor bola<i class="mdi mdi-heart text-danger"></i> 
                             </div>
                         </div>
                     </div>
